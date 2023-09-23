76749aff747fc2134983432bf4d8cf842cad2941
leila <leilamessi96@gmail.com>
2023-09-22 20:45:01 -0500

    create Cento7 with user data

---
+++ lightsailCentos7.tf
@@ -0,0 +1,26 @@
+
+
+resource "aws_lightsail_instance" "centos_server" {
+  name              = "centos-server"
+  availability_zone = "us-east-1a"  
+  blueprint_id      = "centos_7_1901_01"
+
+  user_data = <<-USERDATA
+    #!/bin/bash
+    sudo yum update -y
+    sudo yum install unzip wget httpd -y
+    sudo wget https://github.com/utrains/static-resume/archive/refs/heads/main.zip
+    sudo unzip main.zip
+    sudo rm -rf /var/www/html/*
+    sudo cp -r static-resume-main/* /var/www/html/
+    sudo systemctl start httpd
+    sudo systemctl enable httpd
+
+    # Output the public IP
+    echo "Public IP: \$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)"
+    USERDATA
+}
+
+output "public_ip" {
+  value = aws_lightsail_instance.centos_server.public_ip_address
+}
